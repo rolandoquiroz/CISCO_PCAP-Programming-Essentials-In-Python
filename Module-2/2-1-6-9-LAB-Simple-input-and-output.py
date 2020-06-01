133ea4702845878d994887b89031e65d594492b5
rolandoquiroz <rolandoquiroz_inc@hotmail.com>
2020-06-01 18:25:14 -0500

    Commit #6: Python Simple input simple output LAB

---
+++ Module-2/2-1-6-9-LAB-Simple-input-and-output.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# input a float value for variable a here
+a = float(input('Input a float value for variable a here: '))
+# input a float value for variable b here
+b = float(input('Input a float value for variable b here: '))
+# output the result of addition here
+print('Result of addition here ' + str(a + b))
+# output the result of subtraction here
+print('Result of subsraction here ' + str(a - b))
+# output the result of multiplication here
+print('Result of multiplication here ' + str(a * b))
+# output the result of division here
+print('Result of division here ' + str(a / b))
+print("\nThat's all, folks!")
