07e5d298debe80d8e72741e01f698def9e69722c
rolandoquiroz <rolandoquiroz_inc@hotmail.com>
2020-06-01 21:12:07 -0500

    Commit #9: Python Simple input simple output LAB

--- Module-2/2-1-6-10-LAB-Simple-input-and-output.py
+++ Module-2/2-1-6-10-LAB-Simple-input-and-output.py
@@ -1,4 +1,27 @@
 #!/usr/bin/python3
+"""
+Test Data
+
+Sample input: 1
+
+Expected output:
+y = 0.6000000000000001
+
+Sample input: 10
+
+Expected output:
+y = 0.09901951266867294
+
+Sample input: 100
+
+Expected output:
+y = 0.009999000199950014
+
+Sample input: -5
+
+Expected output:
+y = -0.19258202567760344
+"""
 x = float(input("Enter value for x: "))
 
 # put your code here
