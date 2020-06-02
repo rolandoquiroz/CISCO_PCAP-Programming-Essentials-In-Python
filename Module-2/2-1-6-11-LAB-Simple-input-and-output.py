943b8c2a9ecef57fee45e45eb4c6cd01c503e551
rolandoquiroz <rolandoquiroz_inc@hotmail.com>
2020-06-01 20:59:19 -0500

    Commit #8: Python Simple input simple output LAB

--- Module-2/2-1-6-11-LAB-Simple-input-and-output.py
+++ Module-2/2-1-6-11-LAB-Simple-input-and-output.py
@@ -1,4 +1,36 @@
 #!/usr/bin/python3
+"""
+Your task is to prepare a simple code able to evaluate the end time of
+a period of time, given as a number of minutes (it could be arbitrarily
+large). The start time is given as a pair of hours (0..23) and minutes
+(0..59). The result has to be printed to the console.
+
+For example, if an event starts at 12:17 and lasts 59 minutes,
+it will end at 13:16.
+
+Test Data
+
+Sample input:
+12
+17
+59
+
+Expected output: 13:16
+
+Sample input:
+23
+58
+642
+
+Expected output: 10:40
+
+Sample input:
+0
+1
+2939
+
+Expected output: 1:0
+"""
 hour = int(input("Starting time (hours): "))
 mins = int(input("Starting time (minutes): "))
 dura = int(input("Event duration (minutes): "))
