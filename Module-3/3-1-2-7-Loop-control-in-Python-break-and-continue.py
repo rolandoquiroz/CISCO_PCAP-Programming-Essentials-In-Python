3a1ff2162ff762f480f0cdd8a1f438ffafdaf056
rolandoquiroz <rolandoquiroz_inc@hotmail.com>
2020-06-06 01:55:22 -0500

    Commit # 14:  3.1.2.9 LAB: The break statement - Stuck in a loop

---
+++ Module-3/3-1-2-7-Loop-control-in-Python-break-and-continue.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+break and continue example
+
+    break - exits the loop immediately, and unconditionally ends the loop's
+    operation; the program begins to execute the nearest instruction after
+    the loop's body;
+
+    continue - behaves as if the program has suddenly reached the end of the
+    body; the next turn is started and the condition expression is tested
+    immediately.
+"""
+print("The break instruction:")
+for i in range(1, 6):
+    if i == 3:
+        break
+    print("Inside the loop.", i)
+print("Outside the loop.")
+
+
+# continue - example
+
+print("\nThe continue instruction:")
+for i in range(1, 6):
+    if i == 3:
+        continue
+    print("Inside the loop.", i)
+print("Outside the loop.")
---
+++ Module-3/3-1-2-8-Loop-control-in-Python-break-and-continue.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Program that recognizes the largest among the entered numbers.
+We'll convert it twice, using the break and continue instructions.
+
+The break variant goes here:
+"""
+largestNumber = -99999999
+counter = 0
+
+while True:
+    number = int(input("Enter a number or type -1 to end program: "))
+    if number == -1:
+        break
+    counter += 1
+    if number > largestNumber:
+        largestNumber = number
+
+if counter != 0:
+    print("The largest number is", largestNumber)
+else:
+    print("You haven't entered any number.")
+
+"""
+And now the continue variant:
+"""
+largestNumber = -99999999
+counter = 0
+
+number = int(input("Enter a number or type -1 to end program: "))
+
+while number != -1:
+    if number == -1:
+        continue
+    counter += 1
+
+    if number > largestNumber:
+        largestNumber = number
+    number = int(input("Enter a number or type -1 to end program: "))
+
+if counter:
+    print("The largest number is", largestNumber)
+else:
+    print("You haven't entered any number.")
